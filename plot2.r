b11acaf8e844c77ed735d576e4212183eab06856
mahimatiwari <mahima.indian@gmail.com>
2014-07-14 01:25:07 +0530

    "Assingment1"

--- plot2.r
+++
@@ -1,9 +0,0 @@
-function(){
-        data <- read.csv("household_power_consumption.txt", sep = ";", na.strings = "?")
-		data <- as.data.table(data)
-        good <- (data$"Date" == "1/2/2007") | (data$"Date" == "2/2/2007")
-        data <- data[good]
-        png(file = "plot2.png", width = 480, height = 480)
-        par(bg = "transparent")
-        plot(data$Global_active_power, type = "s", ylab = "Global Active Power (Kilowatts)", xlab = "")
-        dev.off()
